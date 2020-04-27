99f3c410d7d8237cc8825d81f0f98fd220d75041
alexvlife <alexvlife@gmail.com>
2020-04-27 20:56:42 +0300

    realizes a connection to the database (MongoDB)
    - realized connection to MongoDB;
    - added config keys.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan'); // HTTP request logger middleware
@@ -7,17 +8,26 @@ const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
+const keys = require('./config/keys');
 const app = express();
 
 
+// connection to database
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected.'))
+  .catch(error => console.log('ERROR by MongoDB connection:', error));
+
+// for HTTP request logging
 app.use(morgan('dev'));
 
 // for the Express, to parsing the req.body properties as JS-objects
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// CORS
 app.use(cors());
 
+// routes
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://alexv:mongo123@cluster1-0lx3g.mongodb.net/test?retryWrites=true&w=majority'
+};

73ae79a30f55cd5a95c5e96bdf88cc6940ca3180
alexvlife <alexvlife@gmail.com>
2020-04-27 16:06:56 +0300

    creates a server (with automatically restarting)
    - added Express framework package in dependencies;
    - added nodemon tool package in dependencies;
    - created simple server by express;
    - added script "server" to automatically restarting server by nodemon tool.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('it works');
+const app = require('./app');
+
+const PORT_DEFAULT = 5000;
+const port = process.env.port || PORT_DEFAULT;
+
+
+app.listen(port, () => {
+  console.log(`Server has been started on ${port}`);
+});

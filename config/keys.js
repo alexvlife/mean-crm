1d2304c3a1dd6a4b53d479fdb5014e7e42c26028
alexvlife <alexvlife@gmail.com>
2020-04-28 12:15:55 +0300

    auth-backend: login
    - added jsonwebtoken package in dependencies
    (for implementation of JSON Web Tokens.);
    - this package is applied to the project;
    - realized login function by auth controller.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://alexv:mongo123@cluster1-0lx3g.mongodb.net/mean_crm_test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://alexv:mongo123@cluster1-0lx3g.mongodb.net/mean_crm_test?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,53 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys')
 
 
-module.exports.login = function(req, res) {
+module.exports.login = async function(req, res) {
+  const candidate = await User.findOne({ email: req.body.email });
 
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+  if (candidate) {
+    // check password
+    const entryPassword = req.body.password;
+    const passwordWithHash = candidate.password;
+    const passwordResult = bcrypt.compareSync(entryPassword, passwordWithHash);
+
+    if (passwordResult) {
+      // generating a token
+      const SECONDS_IN_HOUR = 60 * 60;
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, { expiresIn: SECONDS_IN_HOUR });
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      });
+    } else {
+      // send error
+      res.status(401).json({
+        message: 'Wrong password.'
+      });
     }
-  });
+
+  } else {
+    // send error
+    res.status(404).json({
+      message: 'User with such email address was not found.',
+    });
+  }
 }
 
 
 module.exports.register = async function(req, res) {
   // check user
-  const candidate = await User.findOne({email: req.body.email});
+  const candidate = await User.findOne({ email: req.body.email });
 
   if (candidate) {
     // send error
     res.status(409).json({
-      message: 'Such email already exist. Try entering a different email'
+      message: 'Such email already exist. Try entering a different email.'
     });
   } else {
     // create new user

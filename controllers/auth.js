55db5b87d0b810b56841dee7e7f5b44f9108702b
alexvlife <alexvlife@gmail.com>
2020-04-28 14:39:21 +0300

    error handler (backend)
    - create errorHandler in /utils

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
 
 
 module.exports.login = async function(req, res) {
@@ -61,8 +62,8 @@ module.exports.register = async function(req, res) {
     try {
       await user.save();
       res.status(201).json(user);
-    } catch {
-      // TODO: handle error
+    } catch(e) {
+      errorHandler(res, e);
     }
   }
 }

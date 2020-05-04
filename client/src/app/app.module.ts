9a13d961a451c70e2fc25fc17cc08a6ae4f4ce96
alexvlife <alexvlife@gmail.com>
2020-05-04 18:40:22 +0300

    front: creates layouts and routes (auth pages)
    - generated layouts in /shared (AuthLayoutComponent, SiteLayoutComponent);
    - generated auth pages (LoginPageComponent, RegisterPageComponent);
    - created auth-layout routes in app-routing.module.ts
    
    Other changes:
    - deleted files app.component.css and app.component.html
    (template mark-up specified in app.component.ts in decorator @Component)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
   ],
   imports: [
     BrowserModule,

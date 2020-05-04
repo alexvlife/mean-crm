29b0a802c4eb58e0c1709541ac7a51b39c4b9a2e
alexvlife <alexvlife@gmail.com>
2020-05-04 21:41:11 +0300

    front: creates login form - part 2: auth service
    - created AuthService in /client/shared/services
    - realized method login in AuthService
    - created file interfaces.ts in /client/shared
    - realized method onSubmit in LoginPageComponent

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators, ValidationErrors } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../shared/services/auth.service';
+import { IUser } from '../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -7,11 +12,35 @@ import { FormControl, FormGroup, Validators, ValidationErrors } from '@angular/f
   styleUrls: ['./login-page.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  authSubscription: Subscription
+
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.initForm();
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        // TODO: write logic (after creating MessageService)
+        // Now you can logIn, using your data.
+
+      } else if (params['accessDenied']) {
+        // TODO: write logic (after creating MessageService)
+        // First you need to logIn.
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
   }
 
   get isEmailControlValueError(): boolean {
@@ -23,6 +52,10 @@ export class LoginPageComponent implements OnInit {
     return Boolean(minLengthError && minLengthError['requiredLength']);
   }
 
+  get isSubmitButtonDisabled(): boolean {
+    return this.form.invalid || this.form.disabled;
+  }
+
   isFormControlRequiredError(formControlName: string): boolean {
     return this.form?.get(formControlName)?.errors['required'];
   }
@@ -32,7 +65,19 @@ export class LoginPageComponent implements OnInit {
   }
 
   onSubmit(): void {
-    // TODO: write logic
+    this.form.disable();
+
+    this.authSubscription = this.authService.login((this.form.value as IUser)).subscribe(
+      () => {
+        // TODO: create route /overview
+
+        // this.router.navigate(['/overview']);
+      },
+      error => {
+        console.warn('Login error:', error);
+        this.form.enable();
+      },
+    );
   }
 
   private initForm(): void {
---
+++ client/src/app/shared/services/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { IUser } from '../interfaces';
+
+interface ILoginResponse {
+  token: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _token: string = null;
+
+  constructor(private http: HttpClient) {}
+
+  register(): void {
+    // TODO: write logic.
+  }
+
+  login(user: IUser): Observable<ILoginResponse> {
+    return this.http.post<ILoginResponse>('/api/auth/login', user)
+      .pipe(
+        tap(
+          ({token}) => {
+            localStorage.setItem('auth-token', token);
+            this.setToken(token);
+          }
+        )
+      );
+  }
+
+  setToken(token: string): void {
+    this._token = token;
+  }
+
+  getToken(): string {
+    return this._token;
+  }
+
+  isAuthenticated(): boolean {
+    return Boolean(this._token);
+  }
+
+  logout(): void {
+    this.setToken(null);
+    localStorage.clear();
+  }
+}

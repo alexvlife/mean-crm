7336a0178f2ec0e46645575f06a265a804b9ecf7
alexvlife <alexvlife@gmail.com>
2020-05-05 17:23:14 +0300

    front: routes guard
    - realized AuthGuard in /client/shared/guards
    - added AuthGuard in canActivate array by SiteLayoutComponent Routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
-import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -15,7 +16,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: '', component: SiteLayoutComponent, children: [
+    path: '', component: SiteLayoutComponent, canActivate: [AuthGuard], children: [
 
     ]
   }

32d72ce299f251b34c00da36e28e06dfab8d7ff5
alexvlife <alexvlife@gmail.com>
2020-06-15 20:27:46 +0300

    front: history-page - view history-list
    - in HistoryListComponent added some logic (for modal view)

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,11 +1,39 @@
-import { Component, Input } from '@angular/core';
-import { IOrder } from 'src/app/shared/interfaces';
+import { Component, Input, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { IOrder, IOrderPosition } from 'src/app/shared/interfaces';
+import { IMaterialInstance, MaterialService } from 'src/app/shared/services/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css']
 })
-export class HistoryListComponent {
+export class HistoryListComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('modal') modalRef: ElementRef;
   @Input() orders: IOrder[];
+
+  selectedOrder: IOrder;
+  modal: IMaterialInstance;
+
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  computePrice(order: IOrder): number {
+    return order.list.reduce((total: number, item: IOrderPosition) => {
+      return total += item.quantity * item.cost;
+    }, 0);
+  }
+
+  selectOrder(order): void {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  closeModal(): void {
+    this.modal.close();
+  }
 }
